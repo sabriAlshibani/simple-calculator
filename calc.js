2409a3b24633076dc94feee321f868422296f3da
sabriAlshibani <sbryalshybany1@gmail.com>
2024-03-19 22:03:14 +0300

    try to make the calculator well

--- calc.js
+++ calc.js
@@ -34,3 +34,65 @@ function calculator() {
 
     }
 }
+function num1() {
+    document.getElementById("result").value = 1;
+    document.querySelectorAll("#button").style.backgroundColor = "green";
+}
+function num2() {
+    document.getElementById("result").value = 2;
+    document.querySelector("#num1").style.backgroundColor = "green";
+
+}
+function num3() {
+    document.getElementById("result").value = 3;
+    document.querySelector("#num1").style.backgroundColor = "green";
+
+}
+function num4() {
+    document.getElementById("result").value = 4;
+    document.querySelector("#num1").style.backgroundColor = "green";
+
+}
+function num5() {
+    document.getElementById("result").value = 5;
+    document.querySelector("#num1").style.backgroundColor = "green";
+
+}
+function num6() {
+    document.getElementById("result").value = 6;
+    document.querySelector("#num1").style.backgroundColor = "green";
+
+}
+function num7() {
+    document.getElementById("result").value = 7;
+    document.querySelector("#num1").style.backgroundColor = "green";
+
+}
+function num8() {
+    document.getElementById("result").value = 8;
+    document.querySelector("#num1").style.backgroundColor = "green";
+
+}
+function num9() {
+    document.getElementById("result").value = 9;
+    document.querySelector("#num1").style.backgroundColor = "green";
+
+}
+function del() {
+    document.getElementById("result").value =8;
+}
+function res() {
+    document.getElementById("result").value = 1;
+}
+function plus() {
+    document.getElementById("result").value = 1;
+}
+function min() {
+    document.getElementById("result").value = 1;
+}
+function no() {
+    document.getElementById("result").value = 1;
+}
+function ne() {
+    document.getElementById("result").value = 1;
+}
